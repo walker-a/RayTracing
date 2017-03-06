60d33aaaf312bfc30b9a6961bd9b25c3ff9fecec
jackoe <jackwines@mac.com>
2017-03-06 14:56:08 -0600

    fixed intersection

--- sphere.c
+++ sphere.c
@@ -4,6 +4,6 @@ typedef struct sphere  {
 }
 
 // returns whether intersection occurs, and if so, writes to a location
-int intersection(int[3] start, int[3] directionVector, int[3] intersectionLocation)  {
+int intersection(sphere *inputSphere, int[3] start, int[3] directionVector, int[3] intersectionLocation)  {
 
 }
