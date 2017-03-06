f9955d4931c8d301da8d19fccc9c0759265b7cf1
jackoe <jackwines@mac.com>
2017-03-06 14:55:08 -0600

    added sphere

---
+++ sphere.c
@@ -0,0 +1,9 @@
+typedef struct sphere  {
+    int[3] center;
+    int radius;
+}
+
+// returns whether intersection occurs, and if so, writes to a location
+int intersection(int[3] start, int[3] directionVector, int[3] intersectionLocation)  {
+
+}
