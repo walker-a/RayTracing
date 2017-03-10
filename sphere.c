81d5c0b161a4611771bc67a0fd159daa4c7c8a1e
jackoe <jackwines@mac.com>
2017-03-09 19:21:52 -0600

    shape struct, now a thing

--- sphere.c
+++
@@ -1,9 +0,0 @@
-typedef struct sphere  {
-    int[3] center;
-    int radius;
-}
-
-// returns whether intersection occurs, and if so, writes to a location
-int intersection(sphere *inputSphere, int[3] start, int[3] directionVector, int[3] intersectionLocation)  {
-
-}
