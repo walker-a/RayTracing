21be67ed747b400f5d2dc97f79a1a20990f5d807
jackoe <jackwines@mac.com>
2017-03-09 23:16:28 -0600

    added, modded

--- shapes.c
+++ shapes.c
@@ -20,7 +20,7 @@ int quadraticFormula (double results[2], double a, double b, double c)  {
 // d is a unit direction vector
 // s is where the ray started
 // taken from https://en.wikipedia.org/wiki/Ray_tracing_(graphics)#Example
-int sphereIntersection(shape *inputSphere, double s[3], double d[3], double intersectionLoc[3], double rgb[3])  {
+int sphereIntersect(shape *inputSphere, double s[3], double d[3], double intersectLoc[3], double rgb[3])  {
     double radius = inputSphere -> unif[3];
     double *center = inputSphere -> unif;
     double quadResults[2];
@@ -40,7 +40,7 @@ int sphereIntersection(shape *inputSphere, double s[3], double d[3], double inte
     }
     double dtScaled[3];
     vecScale(3, t, d, dtScaled);
-    vecAdd(3, s, dtScaled, intersectionLoc);
+    vecAdd(3, s, dtScaled, intersectLoc);
     double sphereColor[3] = {.5, .7, .3};
     vecCopy(3, sphereColor, rgb);
     return 0;
@@ -56,7 +56,7 @@ shape *sphereMalloc()  {
 // sets the shape's unifs. [x, y, z, r]
 void sphereInit(shape *toReturnShape, double center[3], double radius)  {
     toReturnShape -> unifDim = 4;
-    toReturnShape -> intersection = sphereIntersection;
+    toReturnShape -> intersection = sphereIntersect;
     vecCopy(3, center, toReturnShape -> unif);
     toReturnShape -> unif[3] = radius;
 }
