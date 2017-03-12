44affbad95203594a47f04aa79130ebae0caafcc
walkera2 <walkera2@carleton.edu>
2017-03-11 18:23:04 -0600

    keyboard controls via spherical coords

--- mainTracing.c
+++ mainTracing.c
@@ -32,11 +32,78 @@ double target[3];
 double targetToScreen;
 double screenToCam;
 double viewDir[3];
+double viewDirSpherical[3];
 double screenCenter[3];
 double camPos[3];
 // lrVec and udVec must be perpendicular to viewDir
 double lrVec[3];
 double udVec[3];
+double lrVecSpherical[3];
+double udVecSpherical[3];
+
+// converts vector coords to spherical coords
+// stores info in array: array[0] = rho, array[1] = phi, array[2] = theta
+void vecToSpherical(double vec[3], double spherical[3]) {
+    spherical[0] = vecLength(3, vec);
+    spherical[1] = atan2(vec[1], vec[0]);
+    spherical[2] = acos(vec[2] / spherical[0]);
+}
+
+// converts spherical coords to vector coords
+// spherical is rho, phi, theta
+void sphericalToVec(double spherical[3], double vec[3]) {
+    vec[0] = spherical[0] * sin(spherical[1]) * cos(spherical[2]);
+    vec[1] = spherical[0] * sin(spherical[1]) * sin(spherical[2]);
+    vec[2] = spherical[0] * cos(spherical[1]);
+}
+
+// adds phi and theta to the current phi and theta values in viewDir
+// modifies left right vector and up down vectors to account for this as well
+void modViewDir(double phi, double theta) {
+    viewDirSpherical[1] = viewDirSpherical[1] + phi;
+    viewDirSpherical[2] = viewDirSpherical[2] + theta;
+    lrVecSpherical[1] = lrVecSpherical[1] + phi;
+    lrVecSpherical[2] = lrVecSpherical[2] + theta;
+    udVecSpherical[1] = udVecSpherical[1] + phi;
+    udVecSpherical[2] = udVecSpherical[2] + theta;
+    sphericalToVec(viewDirSpherical, viewDir);
+    sphericalToVec(lrVecSpherical, lrVec);
+    sphericalToVec(udVecSpherical, udVec);
+    vecUnit(3, viewDir, viewDir);
+    vecUnit(3, lrVec, lrVec);
+    vecUnit(3, udVec, udVec);
+}
+
+// Move camera on arrow keys + shift + option.
+void handleKeyDown(int key, int shiftIsDown, int controlIsDown,
+        int altOptionIsDown, int superCommandIsDown) {
+    switch(key) {
+        double theta = .1;
+        double phi = .1;
+        case 257:
+        break;
+        case 262:  // right
+        modViewDir(0, theta);
+        break;
+        case 263:  // left
+        modViewDir(0, -theta);
+        break;
+        case 264:  // down
+        if(shiftIsDown)  {
+            targetToScreen = targetToScreen - 1;
+        }  else  {
+            modViewDir(-phi, 0);
+        }
+        break;
+        case 265:  // up
+        if(shiftIsDown)  {
+            targetToScreen = targetToScreen + 1;
+        }  else  {
+            modViewDir(phi, 0);
+        }
+        break;
+    }
+}
 
 int getScreenCoordX(double coord) {
     double ratio = WIDTH / screenWidth;
@@ -137,6 +204,9 @@ void sceneInitialize(double targetPos[3], double targetToScreenDist, double scre
     vecUnit(3, viewDir, viewDir);
     vecUnit(3, lrVec, lrVec);
     vecUnit(3, udVec, udVec);
+    vecToSpherical(viewDir, viewDirSpherical);
+    vecToSpherical(lrVec, lrVecSpherical);
+    vecToSpherical(udVec, udVecSpherical);
     // set targetPos and distances to screen and camera
     target[0] = targetPos[0]; target[1] = targetPos[1]; target[2] = targetPos[2];
     targetToScreen = targetToScreenDist;
@@ -171,5 +241,7 @@ int main(int argc, const char **argv)  {
     pixClearRGB(0.0, 0.0, 0.0);
     initializeShapes();
     launchRays();
+    pixSetKeyDownHandler(handleKeyDown);
+    pixSetKeyRepeatHandler(handleKeyDown);
     pixRun();
 }
